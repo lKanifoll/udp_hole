26b6a4cb3a1b59258078c57fc5c0f7190dd04ce2
Кочнев Сергей Валерьевич <s.kochnev@comitas.ru>
2025-03-16 23:04:05 +0300

    add threads

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use clap::Parser;
 use std::io;
 use std::net::Ipv4Addr;
 //use std::net::SocketAddr;
+use std::sync::Arc;
 use tokio::net::UdpSocket;
+use tokio::spawn;
+use tokio::sync::Mutex;
 
 #[derive(Parser)]
 struct Cli {
@@ -30,13 +33,16 @@ async fn main() -> anyhow::Result<()> {
     );
 
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
+    let socket = Arc::new(Mutex::new(socket));
+    let socket_send = Arc::clone(&socket);
+    let socket_recv = Arc::clone(&socket);
 
     // Register to server
     let mut byte_vec = args.my_name.as_bytes().to_vec();
 
     byte_vec.insert(0, 0x00);
     byte_vec.push(0xFF);
-
+    let socket = socket.lock().await;
     socket.send_to(&byte_vec, (args.server, UDPPORT)).await?;
     //-----------------------
 
@@ -54,22 +60,50 @@ async fn main() -> anyhow::Result<()> {
         let peer_ip = response.text().await?;
         println!("Peers IP: {}", peer_ip);
 
-        socket.send_to(&[0], &peer_ip).await?;
+        //socket.send_to(&[0], &peer_ip).await?;
 
-        loop {
-            if args.client {
-                let stdin = io::stdin();
-                let mut message = String::new();
-                println!("Enter your message to {}: ", args.peer_name);
-                stdin.read_line(&mut message).unwrap();
-                socket.send_to(message.as_bytes(), &peer_ip).await?;
-            } else {
-                let mut buffer = [0; 512];
-                let (length, src) = socket.recv_from(&mut buffer).await?;
-                let received_data = String::from_utf8_lossy(&buffer[..length]);
-                println!("Received from {}: {}", src, received_data);
+        let send_task = spawn(async move {
+            loop {
+                // let stdin = io::stdin();
+                // let mut message = String::new();
+                // println!("Enter your message to {}: ", args.peer_name);
+                // stdin.read_line(&mut message).unwrap();
+
+                let message = "Привет!";
+
+                let _socket_send = socket_send.lock().await;
+                println!("{}", message);
+                match _socket_send.send_to(message.as_bytes(), &peer_ip).await {
+                    Ok(len) => {
+                        println!("Sent {} bytes", len);
+                    }
+                    Err(e) => {
+                        println!("Error receiving: {}", e);
+                    }
+                }
+
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
             }
-        }
+        });
+
+        let recv_task = spawn(async move {
+            loop {
+                let mut buffer = [0; 512];
+                let _socket_recv = socket_recv.lock().await;
+
+                match _socket_recv.recv_from(&mut buffer).await {
+                    Ok((len, peer_ip)) => {
+                        let message = String::from_utf8_lossy(&buffer[..len]);
+                        println!("Received from {}: {}", peer_ip, message);
+                    }
+                    Err(e) => {
+                        println!("Error receiving: {}", e);
+                    }
+                }
+            }
+        });
+
+        let _ = tokio::join!(send_task, recv_task);
     } else {
         println!("Error: {}", response.status());
     }
