189557c4be3f0730a77b2796477aeae63e21a603
Кочнев Сергей Валерьевич <s.kochnev@comitas.ru>
2025-03-18 22:34:22 +0300

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -15,90 +15,99 @@ struct Cli {
     server: Ipv4Addr,
     #[arg(long, default_value = "10")]
     timeout: u8,
-    #[arg(long, default_value = "false")]
-    client: bool,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let args = Cli::parse();
-    println!(
-        "Hi, {}! Connecting to {} through {} with timeout {}.",
-        args.my_name, args.peer_name, args.server, args.timeout
-    );
-
-    let socket = UdpSocket::bind("0.0.0.0:0").await?;
-    let socket = Arc::new(socket);
-    let socket_send = Arc::clone(&socket);
-    let socket_recv = Arc::clone(&socket);
-
-    // Register to server
-    let mut byte_vec = args.my_name.as_bytes().to_vec();
-
+async fn registration(arg: &Cli, socket: &UdpSocket) {
+    let mut byte_vec = arg.my_name.as_bytes().to_vec();
     byte_vec.insert(0, 0x00);
     byte_vec.push(0xFF);
+    match socket.send_to(&byte_vec, (arg.server, 4200)).await {
+        Ok(_) => {}
+        Err(e) => panic!("Failed to send registration: {e:?}"),
+    }
+}
 
-    socket.send_to(&byte_vec, (args.server, 4200)).await?;
-    //-----------------------
-
-    // Get peer IP
-    let get_peer_info_url = format!(
-        "http://{}:{}/api/wait/{}",
-        args.server, 8080, args.peer_name
-    );
-    let params = [("timeout", args.timeout.to_string())];
+async fn get_peer_ip(arg: &Cli, server: &Ipv4Addr) -> Result<String, reqwest::Error> {
+    let get_peer_info_url = format!("http://{}:{}/api/wait/{}", server, 8080, arg.peer_name);
+    let params = [("timeout", arg.timeout.to_string())];
 
     let client = reqwest::Client::new();
     let response = client.get(get_peer_info_url).query(&params).send().await?;
 
     if response.status().is_success() {
-        let peer_ip = response.text().await?;
-        println!("Peers IP: {}", peer_ip);
-
-        let send_task = spawn(async move {
-            loop {
-                let stdin = io::stdin();
-                let mut message = String::new();
-                println!("Enter your message to {}: ", args.peer_name);
-                stdin.read_line(&mut message).unwrap();
-
-                let message = message.trim();
-
-                if !message.is_empty() {
-                    match socket_send.send_to(message.as_bytes(), &peer_ip).await {
-                        Ok(_) => {}
-                        Err(e) => {
-                            println!("Failed to send: {}", e);
-                        }
-                    }
-                } else {
-                    println!("Message is empty. Wrote something.");
-                }
-            }
-        });
-
-        let recv_task = spawn(async move {
-            loop {
-                let mut buffer = [0; 512];
-
-                match socket_recv.recv_from(&mut buffer).await {
-                    Ok((len, _peer_ip)) => {
-                        let message = String::from_utf8_lossy(&buffer[..len]);
-                        println!("Received from: {}", message);
-                        println!("Ctrl + D for write message.");
-                    }
-                    Err(e) => {
-                        println!("Failed to receive: {}", e);
-                    }
-                }
-            }
-        });
-
-        let _ = tokio::join!(send_task, recv_task);
+        return response.text().await;
     } else {
-        println!("Error: {}", response.status());
+        panic!("Error: {}", response.status());
     }
-    //------------
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
+    println!(
+        "\nHi, {}! Connecting to {} through {} with timeout {}.\n",
+        args.my_name, args.peer_name, args.server, args.timeout
+    );
+
+    let socket = UdpSocket::bind("0.0.0.0:0").await?;
+    let socket = Arc::new(socket);
+
+    registration(&args, &socket).await;
+
+    let peer_ip = match get_peer_ip(&args, &args.server).await {
+        Ok(ip) => ip,
+        Err(e) => panic!("Failed to get peer IP: {}", e),
+    };
+    //println!("Peers IP: {}", peer_ip.as_str());
+
+    // Sending task
+    println!("Enter your message to {}: ", args.peer_name);
+    let socket_send = Arc::clone(&socket);
+    let send_task = spawn(async move {
+        loop {
+            let stdin = io::stdin();
+            let mut message = String::new();
+
+            stdin.read_line(&mut message).unwrap();
+
+            let message = message.trim();
+
+            if !message.is_empty() {
+                match socket_send
+                    .send_to(message.as_bytes(), peer_ip.as_str())
+                    .await
+                {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Failed to send: {}", e);
+                    }
+                }
+            } else {
+                println!("Message is empty. Wrote something.");
+            }
+        }
+    });
+
+    // Receiving task
+    let socket_recv = Arc::clone(&socket);
+    let recv_task = spawn(async move {
+        loop {
+            let mut buffer = [0; 512];
+
+            match socket_recv.recv_from(&mut buffer).await {
+                Ok((len, _peer_ip)) => {
+                    let message = String::from_utf8_lossy(&buffer[..len]);
+                    println!("-->: {}", message);
+                }
+                Err(e) => {
+                    println!("Failed to receive: {}", e);
+                }
+            }
+        }
+    });
+
+    let _ = tokio::join!(send_task, recv_task);
 
     Ok(())
 }
